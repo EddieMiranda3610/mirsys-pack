bba9c5547b969d2200ecacbacc5c54a60d788a6d
Eddie Miranda <87393937+EddieMiranda3610@users.noreply.github.com>
2022-12-20 01:11:53 -0600

    Update main.py
    Rewrote attribute information

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ async def on_ready():
       m[str(member.id)] = {"xp" : 0, "messageCountdown" : 0}
 
   print('Now logged in as {0.user}'.format(client))
-  print('Miranda System Bot Version Python Mode 0.3.0')
+  print('Miranda Notifier Version Python Mode 0.3.0')
   print('(C) 2018 - 2021 Eddie Miranda.')
   print('No part of this program shall be copied, modified, transmitted, reused, or otherwise distributed without permission from either copyright owner.')
   print('This application is currently under development. If you would like to assist in the process: feel free to message us on Discord.')
   print('Our Discord Handles:')
-  print('EddieMiranda&Keira2000#1959')
+  print('EddieMiranda2000#1959')
   while True:
     try:
       for member in client.get_guild(851474242402385970).members:
@@ -56,7 +56,7 @@ async def on_ready():
       }
     elif message.content.startswith('ms;level-xp'):
       await message.content.send([str(message.author)], ': you currently at level ', ["level"], ' and need ' [100 - "xp"], ' to advacnce to the next level.')
-    elif message.content.startswith('lz;cmds'):
+    elif message.content.startswith(',s;cmds'):
       {
         await message.content.send('Here are the commands: \n''ms;cmds -- Prints this command list.\n' "ms;hello -- Makes Lauren say 'Hello there' back to the member.\n" "ms;level-xp -- Shows the member's level and amount of experience (XP) needed to advance to the next level.\n" "ms;updatexp - Updates the experience of all members of the server (Bot Owner Only)")
       }
